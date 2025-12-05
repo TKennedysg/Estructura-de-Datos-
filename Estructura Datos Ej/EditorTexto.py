ef0b30b8461a249d5c61b5b96dd48337cac10c1a
TKennedysg <santi.alvmen@gmail.com>
2025-12-05 07:15:49 -0500

    Update EditorTexto.py

--- Estructura Datos Ej/EditorTexto.py
+++ Estructura Datos Ej/EditorTexto.py
@@ -7,15 +7,14 @@ class Editor:
     def insert(self, new_text):
         self.undo_stack.append(("insertar", len(new_text)))
         self.redo_stack.clear()
-
         self.text += new_text
         print(f"[Insertar] = {self.text}")
 
     def delete(self, length):
+        length = min(length, len(self.text))
         deleted = self.text[-length:]
-        self.undo_stack.append(("Eliminar", deleted))
+        self.undo_stack.append(("eliminar", deleted))
         self.redo_stack.clear()
-
         self.text = self.text[:-length]
         print(f"[Eliminar] = {self.text}")
 
@@ -23,47 +22,45 @@ class Editor:
         if not self.undo_stack:
             print("Nada que deshacer.")
             return
-
         action, value = self.undo_stack.pop()
-
         if action == "insertar":
             deleted = self.text[-value:]
             self.text = self.text[:-value]
             self.redo_stack.append(("insertar", deleted))
-
         elif action == "eliminar":
             self.text += value
             self.redo_stack.append(("eliminar", len(value)))
-
-        print(f"[desahacer] → {self.text}")
+        print(f"[Deshacer] = {self.text}")
 
     def redo(self):
         if not self.redo_stack:
             print("Nada que rehacer.")
             return
-
         action, value = self.redo_stack.pop()
-
         if action == "insertar":
             self.text += value
             self.undo_stack.append(("insertar", len(value)))
-
         elif action == "eliminar":
-            deleted = self.text[-value:]
-            self.text = self.text[:-value]
+            length = min(value, len(self.text))
+            deleted = self.text[-length:]
+            self.text = self.text[:-length]
             self.undo_stack.append(("eliminar", deleted))
-
-        print(f"[Rehacer] → {self.text}")
+        print(f"[Rehacer] = {self.text}")
 
     def show(self):
         print(f"Texto actual: {self.text}")
 
 editor = Editor()
-
 editor.insert("Hola")
 editor.insert(" Mundo")
 editor.delete(6)
 editor.undo()
 editor.undo()
 editor.redo()
+editor.show()
+editor.redo()
+editor.redo()
+editor.redo()
+editor.show()
+editor.undo()
 editor.show()
